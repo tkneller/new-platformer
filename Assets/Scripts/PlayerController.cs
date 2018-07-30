186666c733fdc82641928e1f31a7d5cd3e5d1e1d
tkneller <t.kneller@mail.de>
2018-07-30 21:41:44 +0200

    Wall slide added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private bool facingRight = true;
     public float horizontalVelocityMax = 10f;
     private float horizontalVelocity;
+    private float verticalVelocity = 0;
     public float horizontalAcceleration = 0.5f;
     private float horizontalAccelerationTime = 0;
     public float horizontalFriction = 0.2f;
@@ -22,6 +23,11 @@ public class PlayerController : MonoBehaviour
     private float onGroundRemember;
     public float onGroundRememberTime = 0.2f;
 
+    private bool onWall = false;
+    public Transform wallCheck;
+    public float wallCheckRadius = 0.2f;
+    public float wallSlideMultiplier = 0.5f;
+
     public float jumpVelocity = 10f;
     public float cutJumpHeight = 0.5f; 
     private float jumpPressedRemember;
@@ -35,27 +41,46 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate() {
         onGround = IsOnGround();
+        onWall = IsOnWall();
 
         Move();
     }
 
     void Update () {
         Jump();
+        WallJump();
     }
+
+    void OnGUI() {
+        GUI.Label(new Rect(50, 10, 300, 20), "Horizontal velocity: " + rigid2D.velocity.x);
+        GUI.Label(new Rect(50, 25, 300, 20), "Vertical velocity: " + rigid2D.velocity.y);
+        
+        GUI.Label(new Rect(50, 45, 300, 20), "On ground: " + onGround);
+        GUI.Label(new Rect(50, 60, 300, 20), "On wall: " + onWall);
+    }
+
 
     // Checks if the player is on ground
     private bool IsOnGround() {
         return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
     }
 
+    // Checks if the player is on a wall
+    private bool IsOnWall() {
+        return Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, ground);
+    }
+
     // Movement controlls
     private void Move() {
         moveInput = Input.GetAxisRaw("Horizontal");
-        
+        //moveInput = 1;
+
+
         // Acceleration
         if (moveInput != 0) {
             horizontalAccelerationTime = horizontalAccelerationTime + Time.deltaTime;
-            horizontalVelocity = (((horizontalAcceleration - horizontalFriction) * (horizontalAccelerationTime * moveInput)) + rigid2D.velocity.x);        
+            horizontalVelocity = (horizontalAcceleration - horizontalFriction) * (horizontalAccelerationTime * moveInput) + rigid2D.velocity.x;
+  
         } else {
             horizontalAccelerationTime = 0;
         }
@@ -65,9 +90,8 @@ public class PlayerController : MonoBehaviour
             horizontalVelocity = moveInput * horizontalVelocityMax;
         }
 
-        // Movement decreases slowly over time due to friction if
-        // directional buttons are released
-        if (moveInput == 0 && horizontalVelocity != 0 ) {
+        // Stopping
+        if (Mathf.Abs(moveInput) < 0.01f) {
 
             horizontalFrictionTime = horizontalFrictionTime + Time.deltaTime;
 
@@ -85,8 +109,8 @@ public class PlayerController : MonoBehaviour
                 } else {
                     horizontalVelocity = (horizontalFrictionStopping * horizontalFrictionTime) + rigid2D.velocity.x;
                 }
-            } 
-        }
+            }
+        }   
 
         rigid2D.velocity = new Vector2(horizontalVelocity, rigid2D.velocity.y);
 
@@ -129,6 +153,14 @@ public class PlayerController : MonoBehaviour
             jumpPressedRemember = 0;
             rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpVelocity);
         }
+    }
+
+    // Wall jump / slide controlls
+    private void WallJump() {
+            if (onWall && !onGround &&) {
+                verticalVelocity = rigid2D.velocity.y * wallSlideMultiplier;
+                rigid2D.velocity = new Vector2(rigid2D.velocity.x, verticalVelocity);
+            }
     }
 
     // Flips the sprite according to the direction the player is facing
